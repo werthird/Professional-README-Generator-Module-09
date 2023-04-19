de85d7855c1e2e4be0dd83d72d04dd4a4a03ddf1
Devin Reilly <werthird2016@gmail.com>
2023-04-18 23:16:16 -0400

    imported modules; built questions array; planned layout

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,84 @@
 // TODO: Include packages needed for this application
+const genMark = require('./utils/generateMarkdown.js');
+const fs = require('fs');
+const inquirer = require('inquirer');
+// generateMarkdown, fs, inquirer@8.2.4
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: 'Enter the title for your project.',
+    name: 'title'
+  },
+  {
+    type: 'editor',
+    message: 'Provide a short description. NOTE: Save the file when finished!',
+    name: 'description'
+  },
+  {
+    type: 'input',
+    message: 'Define the current status.',
+    name: 'status'
+  },
+  {
+    type: 'input',
+    message: 'Enter any installation instructions.',
+    name: 'installation'
+  },
+  {
+    type: 'input',
+    message: 'Provide any usage information.',
+    name: 'usage'
+  },
+  {
+    type: 'list',
+    message: 'Select a license type.',
+    name: 'license',
+    choices: ['Apache', 'MIT', 'BSD', 'Unlicensed'],
+  },
+  {
+    type: 'input',
+    message: 'Provide any contributing guidelines for other developers.',
+    name: 'contributing'
+  },
+  {
+    type: 'input',
+    message: 'Provide any test instructions for the users.',
+    name: 'test'
+  },
+  {
+    type: 'input',
+    message: 'List the authors of this project.',
+    name: 'author'
+  },
+  {
+    type: 'input',
+    message: 'Also, list any acknowledgments for this project.',
+    name: ''
+  },
+  {
+    type: 'input',
+    message: 'Provide your GitHub username.',
+    name: 'username'
+  },
+];
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    // return the writeFile method using fileName and data being passed in
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    // inquirer goes in here -> questions passed as argument --- .then call back function (writeToFile(){}) will will use generateMarkdown as data source --- pass data from inquirer into this!! how is spread operator to be used in this?
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  // this will use data passed in from generateMarkdown function. have a logic gate to make sure a license was chosen.
+  return `https://img.shields.io/badge/license-${license}-blue`
+}
 
-// TODO: Create a function that returns the license link
+// TODO: Create a function that returns the license link - this is internally in the .md file...a link to the license section
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  // this will use data passed in from generateMarkdown function. have a logic gate to make sure a license was chosen.
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  // this will use data passed in from generateMarkdown function.  have a logic gate to make sure a license was chosen.
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+    
 `;
+  // you will need to use the license functions in here ^, passing data to the functions you call -- licenseFunction(data.license)
 }
 
 module.exports = generateMarkdown;
